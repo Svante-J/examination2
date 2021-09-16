3a147ed6372cd3d5b7992ea2615892fad2b15c58
Svante-J <72524651+Svante-J@users.noreply.github.com>
2021-09-16 15:51:32 +0200

    mappstruktur o react dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import {MyFirstComponent} from "./components/myfirstcomponent/MyFirstComponent"
 
 function App() {
   return (
     <div>
       <h1> hello wjörld</h1>
+      < MyFirstComponent name="Svante" age ="37"/>
     </div>
   );
 }
---
+++ src/components/myfirstcomponent/MyFirstComponent.js
@@ -0,0 +1,15 @@
+
+import "./MyFirstComponent.css"
+
+export const MyFirstComponent = (props) => {
+
+return(
+    <div>
+        <h1 className="title"> 
+        {props.name}
+        {props.age}
+        </h1>
+    </div>
+)
+
+}
